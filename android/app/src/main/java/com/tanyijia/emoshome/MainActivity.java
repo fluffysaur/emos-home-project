098e3c1681e9f9eb90b2c7302eb164024e261b8d
Tan Yi Jia <tanyijia@gmail.com>
2025-03-03 20:32:31 +0800

    build: add capacitor

---
+++ android/app/src/main/java/com/tanyijia/emoshome/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tanyijia.emoshome;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tanyijia.emoshome',
+  appName: 'eMOS Home',
+  webDir: 'dist'
+};
+
+export default config;
