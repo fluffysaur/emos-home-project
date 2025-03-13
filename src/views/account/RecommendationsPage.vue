25d08542aaa488fe5fd0482696c8797eb1425cfb
Tan Yi Jia <tanyijia@gmail.com>
2025-03-13 23:01:23 +0800

    fix: styling for recommended pages

--- src/views/account/RecommendationsPage.vue
+++ src/views/account/RecommendationsPage.vue
@@ -6,31 +6,38 @@
       <p class="text-lg font-semibold mb-4">Your doctor's recommendations</p>
       <ion-card class="bg-white rounded-lg mb-4">
         <ion-card-content>
-          <div class="text-zinc-800">
+          <ion-text color="dark">
             <FoodList
               title="Dietary Restrictions"
               :items="authStore.$state.user?.dietaryRestrictions || []"
               :iconUrl="'https://cdn.builder.io/api/v1/image/assets/TEMP/dff5e70fe870e55133e0f5c543fa56963fa6f0edf7e923acb44eff602994bdfb?placeholderIfAbsent=true&apiKey=abeb1f4ba85b45269a6aeb4e1c98d24d'"
             />
-          </div>
+          </ion-text>
         </ion-card-content>
       </ion-card>
+
       <ion-card class="bg-white rounded-lg">
         <ion-card-content>
-          <div class="text-zinc-800">
+          <ion-text color="dark">
             <FoodList
               title="Foods you should eat more"
               :items="recommendedFoods"
               :iconUrl="'https://cdn.builder.io/api/v1/image/assets/TEMP/dff5e70fe870e55133e0f5c543fa56963fa6f0edf7e923acb44eff602994bdfb?placeholderIfAbsent=true&apiKey=abeb1f4ba85b45269a6aeb4e1c98d24d'"
             />
+          </ion-text>
+        </ion-card-content>
+      </ion-card>
 
+      <ion-card class="bg-white rounded-lg">
+        <ion-card-content>
+          <ion-text color="dark">
             <FoodList
               class="mt-5"
               title="Foods you should avoid"
               :items="foodsToAvoid"
               :iconUrl="'https://cdn.builder.io/api/v1/image/assets/TEMP/dff5e70fe870e55133e0f5c543fa56963fa6f0edf7e923acb44eff602994bdfb?placeholderIfAbsent=true&apiKey=abeb1f4ba85b45269a6aeb4e1c98d24d'"
             />
-          </div>
+          </ion-text>
         </ion-card-content>
       </ion-card>
     </section>
@@ -40,18 +47,18 @@
 <script setup lang="ts">
 import BackButton from '@/components/common/BackButton.vue'
 import FoodList from '@/components/onboarding/FoodList.vue'
-import { IonContent, IonCard, IonCardContent } from '@ionic/vue'
+import { IonContent, IonCard, IonCardContent, IonText } from '@ionic/vue'
 import { useAuthStore } from '@/stores/authStore'
 
 const authStore = useAuthStore()
 
 const recommendedFoods = [
-  'fruits and vegetables',
-  'whole grains',
-  'lean protein',
-  'low-fat dairy',
-  'calcium-rich foods',
+  'Fruits and vegetables',
+  'Whole grains',
+  'Lean protein',
+  'Low-fat dairy',
+  'Calcium-rich foods',
 ]
 
-const foodsToAvoid = ['saturated and trans fats', 'foods high in salt and sugar', 'alcohol']
+const foodsToAvoid = ['Saturated and trans fats', 'Foods high in salt and sugar', 'Alcohol']
 </script>
--- src/views/onboarding/OnboardingRecommendations.vue
+++ src/views/onboarding/OnboardingRecommendations.vue
@@ -6,22 +6,40 @@
         <ion-grid class="mx-6 mb-4 flex flex-col justify-between">
           <div>
             <div class="text-lg font-semibold mb-4">Your doctor's recommendations</div>
+            <ion-card class="bg-white rounded-lg mb-4">
+              <ion-card-content>
+                <ion-text color="dark">
+                  <FoodList
+                    title="Dietary Restrictions"
+                    :items="authStore.$state.user?.dietaryRestrictions || []"
+                    :iconUrl="'https://cdn.builder.io/api/v1/image/assets/TEMP/dff5e70fe870e55133e0f5c543fa56963fa6f0edf7e923acb44eff602994bdfb?placeholderIfAbsent=true&apiKey=abeb1f4ba85b45269a6aeb4e1c98d24d'"
+                  />
+                </ion-text>
+              </ion-card-content>
+            </ion-card>
+
             <ion-card class="bg-white rounded-lg">
               <ion-card-content>
-                <div class="text-zinc-800">
+                <ion-text color="dark">
                   <FoodList
                     title="Foods you should eat more"
                     :items="recommendedFoods"
                     :iconUrl="'https://cdn.builder.io/api/v1/image/assets/TEMP/dff5e70fe870e55133e0f5c543fa56963fa6f0edf7e923acb44eff602994bdfb?placeholderIfAbsent=true&apiKey=abeb1f4ba85b45269a6aeb4e1c98d24d'"
                   />
+                </ion-text>
+              </ion-card-content>
+            </ion-card>
 
+            <ion-card class="bg-white rounded-lg">
+              <ion-card-content>
+                <ion-text color="dark">
                   <FoodList
                     class="mt-5"
                     title="Foods you should avoid"
                     :items="foodsToAvoid"
                     :iconUrl="'https://cdn.builder.io/api/v1/image/assets/TEMP/dff5e70fe870e55133e0f5c543fa56963fa6f0edf7e923acb44eff602994bdfb?placeholderIfAbsent=true&apiKey=abeb1f4ba85b45269a6aeb4e1c98d24d'"
                   />
-                </div>
+                </ion-text>
               </ion-card-content>
             </ion-card>
           </div>
@@ -36,17 +54,28 @@
 import { useRouter } from 'vue-router'
 import BackButton from '@/components/common/BackButton.vue'
 import FoodList from '@/components/onboarding/FoodList.vue'
-import { IonPage, IonButton, IonContent, IonCard, IonCardContent, IonGrid } from '@ionic/vue'
+import { useAuthStore } from '@/stores/authStore'
+import {
+  IonPage,
+  IonButton,
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonGrid,
+  IonText,
+} from '@ionic/vue'
+
+const authStore = useAuthStore()
 
 const recommendedFoods = [
-  'fruits and vegetables',
-  'whole grains',
-  'lean protein',
-  'low-fat dairy',
-  'calcium-rich foods',
+  'Fruits and vegetables',
+  'Whole grains',
+  'Lean protein',
+  'Low-fat dairy',
+  'Calcium-rich foods',
 ]
 
-const foodsToAvoid = ['saturated and trans fats', 'foods high in salt and sugar', 'alcohol']
+const foodsToAvoid = ['Saturated and trans fats', 'Foods high in salt and sugar', 'Alcohol']
 
 const router = useRouter()
 
